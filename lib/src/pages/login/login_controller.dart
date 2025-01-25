5d3f2551a8c7049429002269ca351a8321e266bb
Leticia Peña <leticia.penhag@fpuna.edu.py>
2025-01-25 15:20:22 -0300

    se comenta el código del login_c y se realizan algunos cambios

--- lib/src/pages/login/login_controller.dart
+++ lib/src/pages/login/login_controller.dart
@@ -14,31 +14,36 @@ class LoginController {
   UserProvider usersProvider = new UserProvider();
   final SharedPreferencesHelper _sharedPreferencesHelper = SharedPreferencesHelper();
 
+  // Inicializa el controlador y verifica si el usuario ya tiene una sesión activa.
   Future init(BuildContext context) async {
     this.context = context;
     await usersProvider.init(context);
-    // si es null se envía vacío
+
+    /* Intenta recuperar el usuario almacenado en el sharedpreferences, tiene en cuenta
+    en este caso la llave 'user'*/
     User user = User.fromJson(await _sharedPreferencesHelper.readSessionToken('user') ?? {});
 
-    print('Usuario: ${user.toJson()}');
+    //print('Usuario: ${user.toJson()}');
 
-    if(user?.sessionToken != null) {
+    // Si el token de sesión no es nulo, redirige al usuario al home
+    if (user?.sessionToken != null) {
       Navigator.pushNamedAndRemoveUntil(context, 'client/products/list', (route) => false);
     }
   }
 
+  // Navega a la página de registro.
   void goToRegisterPage() {
     Navigator.pushNamed(context, 'register');
   }
 
+  // Maneja el proceso de inicio de sesión.
   void login() async {
     String email = emailController.text.trim();
     String password = passwordController.text.trim();
 
     try {
+      // Llama al provider de usuarios para autenticar al usuario.
       ResponseApi? responseApi = await usersProvider.login(email, password);
-
-      //print(responseApi?.data);
 
       if (responseApi == null) {
         SnackbarHelper.show(
@@ -49,6 +54,7 @@ class LoginController {
         return;
       }
 
+      // Muestra un mensaje dependiendo del éxito o fracaso del login.
       SnackbarHelper.show(
         context: context,
         message: responseApi.message,
@@ -56,12 +62,13 @@ class LoginController {
       );
 
       if (responseApi.success) {
-        // Guarda el token de sesión
+        // Si el login es exitoso, convierte los datos de respuesta en un objeto User.
         User user = User.fromJson(responseApi.data);
 
-        //'user' es la llave con la que se comprueba si ya están guardados los datos
+        // Guarda los datos del usuario en sharedpreferences clave 'user'.
         _sharedPreferencesHelper.saveSessionToken('user', user.toJson());
 
+        // Redirige al usuario al home.
         Navigator.pushNamedAndRemoveUntil(context, 'client/products/list', (route) => false);
 
       } else {
--- lib/src/utils/shared_preferences_helper.dart
+++ lib/src/utils/shared_preferences_helper.dart
@@ -2,24 +2,32 @@ import 'dart:convert';
 
 import 'package:shared_preferences/shared_preferences.dart';
 
+/*
+* Permite almacenar y recuperar información, como tokens de sesión o datos de usuario.
+* Usa json.encode y json.decode para manejar datos complejos como mapas.*/
+
 class SharedPreferencesHelper {
+  // Guarda un valor codificado en JSON en las preferencias compartidas usando una clave específica.
   Future<void> saveSessionToken(String key, value) async {
     final preferences = await SharedPreferences.getInstance();
     preferences.setString(key, json.encode(value));
   }
 
+  // Lee un valor almacenado codificado en JSON y lo decodifica a su formato original.
   Future<dynamic> readSessionToken(String key) async {
     final preferences = await SharedPreferences.getInstance();
     final storedValue = preferences.getString(key);
     if (storedValue == null) return null;
-    return json.decode(storedValue);
+    return json.decode(storedValue); // Decodifica el valor JSON.
   }
 
+  // Comprueba si una clave específica existe en las preferencias compartidas.
   Future<bool> containsSessionToken(String key) async {
     final preferences = await SharedPreferences.getInstance();
     return preferences.containsKey(key);
   }
 
+  // Elimina un valor almacenado asociado con una clave específica.
   Future<bool> clearSession(String key) async {
     final preferences = await SharedPreferences.getInstance();
     return preferences.remove(key);
