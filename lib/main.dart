212215aa2b1d8430dc858bdc487c22ce164a759d
Leticia Peña <leticia.penhag@fpuna.edu.py>
2025-01-28 23:08:11 -0300

    se realizan algunos cambios en main.dart y se crea una pagina de roles en el caso que el usuario tenga más de un rol

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'package:lomi_chef_to_go/src/roles/roles_page.dart';
 
 import 'src/utils/app_colors.dart';
 
-void main() {
+/*void main() {
   runApp(const MyApp());
 }
 
@@ -33,6 +33,65 @@ class _MyAppState extends State<MyApp> {
         'delivery/orders/list' : (BuildContext context) => DeliveryOrdersListPage(),
         'restaurant/orders/list' : (BuildContext context) => RestaurantOrdersListPage(),
         'roles' : (BuildContext context) => RolesPage(),
+      },
+      theme: ThemeData(
+        primaryColor: AppColors.primaryColor,
+        appBarTheme: AppBarTheme(
+          backgroundColor: AppColors.primaryColor,
+        ),
+      ),
+    );
+  }
+}*/
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fast Food Delivery - Lomi Chef',
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'login',
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case 'login':
+            return MaterialPageRoute(builder: (_) => LoginPage());
+          case 'register':
+            return MaterialPageRoute(builder: (_) => RegisterPage());
+          case 'client/products/list':
+            return MaterialPageRoute(builder: (_) => ClientProductsListPage());
+          case 'delivery/orders/list':
+            return MaterialPageRoute(builder: (_) => DeliveryOrdersListPage());
+          case 'restaurant/orders/list':
+            return MaterialPageRoute(builder: (_) => RestaurantOrdersListPage());
+          case 'roles':
+          // Se obtienen los parámetros para RolesPage
+            final arguments = settings.arguments as List<Map<String, dynamic>>?;
+            final roles = arguments ?? [
+              {"id": 1, "name": "CLIENTE", "route": "client/products/list"},
+              {"id": 2, "name": "RESTAURANTE", "route": "restaurant/orders/list"},
+              {"id": 3, "name": "REPARTIDOR", "route": "delivery/orders/list"},
+            ];
+            return MaterialPageRoute(
+              builder: (_) => RolesPage(roles: roles),
+            );
+          default:
+            return MaterialPageRoute(
+              builder: (_) => const Scaffold(
+                body: Center(child: Text('Página no encontrada')),
+              ),
+            );
+        }
       },
       theme: ThemeData(
         primaryColor: AppColors.primaryColor,
--- lib/src/roles/roles_page.dart
+++ lib/src/roles/roles_page.dart
@@ -1,4 +1,53 @@
 import 'package:flutter/material.dart';
+
+class RolesPage extends StatelessWidget {
+  final List<Map<String, dynamic>> roles;
+
+  const RolesPage({super.key, required this.roles});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Selecciona tu Rol'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 16.0,
+            mainAxisSpacing: 16.0,
+          ),
+          itemCount: roles.length,
+          itemBuilder: (BuildContext context, int index) {
+            final role = roles[index];
+            return GestureDetector(
+              onTap: () {
+                Navigator.pushNamed(context, role['route']);
+              },
+              child: Card(
+                elevation: 4,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Center(
+                  child: Text(
+                    role['name'],
+                    style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+
+/*import 'package:flutter/material.dart';
 class RolesPage extends StatefulWidget {
   const RolesPage({super.key});
 
@@ -13,4 +62,4 @@ class _RolesPageState extends State<RolesPage> {
       child:Text('ROLES PAGE')
     );
   }
-}
+}*/
