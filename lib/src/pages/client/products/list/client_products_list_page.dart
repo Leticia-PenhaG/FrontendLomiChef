426e4d1c612d889f35c287fcf032e29f840ffde9
Leticia Peña <leticia.penhag@fpuna.edu.py>
2025-03-23 12:55:25 -0300

    se actualiza el card de producto

--- lib/src/pages/client/products/list/client_products_list_page.dart
+++ lib/src/pages/client/products/list/client_products_list_page.dart
@@ -32,12 +32,6 @@ class _ClientProductsListPageState extends State<ClientProductsListPage> {
     });
   }
 
-  //     setState(() {
-  //       _isInitialized = true; // Cambia el estado una vez que `context` está listo
-  //     });
-  //   });
-  // }
-
   @override
   Widget build(BuildContext context) {
     if (!_isInitialized) {
@@ -52,12 +46,12 @@ class _ClientProductsListPageState extends State<ClientProductsListPage> {
       );
     }
 
-    return DefaultTabController(  // Faltaba envolver con DefaultTabController
+    return DefaultTabController(
       length: _controllerClient.categories.length,
       child: Scaffold(
         key: _controllerClient.key,
         appBar: PreferredSize(
-          preferredSize: Size.fromHeight(170),
+          preferredSize: Size.fromHeight(180),
           child: AppBar(
             automaticallyImplyLeading: false,
             backgroundColor: Colors.white,
@@ -68,6 +62,7 @@ class _ClientProductsListPageState extends State<ClientProductsListPage> {
               children: [
                 SizedBox(height: 70),
                 _menuDrawer(),
+                SizedBox(height: 15),
                 _textFieldSearch(),
                 SizedBox(height: 20),
               ],
@@ -111,81 +106,299 @@ class _ClientProductsListPageState extends State<ClientProductsListPage> {
     );
   }
 
+  //opcion 1
   Widget _cardProduct(Product product) {
     return Container(
-      height: 250,
+      margin: EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            blurRadius: 5,
+            spreadRadius: 2,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
       child: Card(
-        elevation: 3.0,
+        elevation: 0,
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15)
+          borderRadius: BorderRadius.circular(15),
         ),
-        child: Stack(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Positioned(
-                top: -1.0,
-                right: -1.0,
+            Expanded(
+              flex: 8, // Se aumenta la altura para que la imagen tenga más espacio
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(15),
+                  topRight: Radius.circular(15),
+                ),
                 child: Container(
-                  width: 40,
-                  height: 40,
-                  decoration: BoxDecoration(
-                    color: AppColors.primaryColor,
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(15),
-                      topRight: Radius.circular(20)
-                    )
+                  width: double.infinity,
+                  color: Colors.white, // Fondo blanco en caso de imagen nula
+                  child: product.image1 != null && product.image1!.isNotEmpty
+                      ? FadeInImage(
+                    image: NetworkImage(product.image1!),
+                    placeholder: AssetImage('assets/img/loading.png'),
+                    fit: BoxFit.contain, // Para que la imagen no se recorte
+                    fadeInDuration: Duration(milliseconds: 300),
+                    imageErrorBuilder: (context, error, stackTrace) {
+                      return Image.asset(
+                        'assets/img/no-image-icon.png',
+                        fit: BoxFit.contain,
+                      );
+                    },
+                  )
+                      : Image.asset(
+                    'assets/img/no-image-icon.png',
+                    fit: BoxFit.contain,
                   ),
-                  child: Icon(Icons.add, color: Colors.white),
-                )
+                ),
+              ),
             ),
-            Column(
-              //el recycler view que hace que se carguen los productos por categoría que hay en la base de datos
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  height: 150,
-                  margin: EdgeInsets.only(top: 20),
-                  width: MediaQuery.of(context).size.width * 0.45,
-                  padding: EdgeInsets.all(10),
-                  child: FadeInImage(
-                      image: product.image1 != null
-                        ? NetworkImage(product.image1!)
-                        : AssetImage('assets/img/burger.png'),
-                      fit: BoxFit.contain,
-                      fadeInDuration: Duration(milliseconds: 50),
-                      placeholder: AssetImage('assets/img/no-image-icon.png'),
-                  ),
+            SizedBox(height: 10), // Espacio antes del nombre
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                product.name ?? 'Sin nombre',
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
                 ),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 20),
-                  height: 37,
-                  child: Text(
-                    product.name ?? '',
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      fontSize: 15,
-                      //fontStyle: 'Nimbusans'
-                    ),
-                  ),
+              ),
+            ),
+            SizedBox(height: 5),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                '${product.price ?? 0} Gs.',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.green[700],
+                  fontWeight: FontWeight.w600,
                 ),
-                Spacer(),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 20),
-                  child: Text('${product.price ?? 0} \Gs.',
-                    style: TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold
-                      //fontFamily: ''
+              ),
+            ),
+            Spacer(), // Asegura que el botón quede alineado abajo
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Container(
+                margin: EdgeInsets.only(right: 10, bottom: 10),
+                decoration: BoxDecoration(
+                  color: AppColors.primaryColor,
+                  shape: BoxShape.circle,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 4,
+                      offset: Offset(0, 2),
                     ),
-                  ),
-                )
-              ],
-            )
+                  ],
+                ),
+                child: IconButton(
+                  icon: Icon(Icons.add, color: Colors.white, size: 20), // Tamaño menor
+                  onPressed: () {
+                    // Acción de agregar producto
+                  },
+                ),
+              ),
+            ),
           ],
         ),
       ),
     );
   }
+
+  //Forma 2
+  // Widget _cardProduct(Product product) {
+  //   return Container(
+  //     margin: EdgeInsets.all(8),
+  //     decoration: BoxDecoration(
+  //       borderRadius: BorderRadius.circular(15),
+  //       boxShadow: [
+  //         BoxShadow(
+  //           color: Colors.grey.withOpacity(0.2),
+  //           blurRadius: 5,
+  //           spreadRadius: 2,
+  //           offset: Offset(0, 3),
+  //         ),
+  //       ],
+  //     ),
+  //     child: Card(
+  //       elevation: 0,
+  //       shape: RoundedRectangleBorder(
+  //         borderRadius: BorderRadius.circular(15),
+  //       ),
+  //       child: Column(
+  //         crossAxisAlignment: CrossAxisAlignment.start,
+  //         children: [
+  //           Expanded(
+  //             flex: 8, // Se aumenta la altura para que la imagen tenga más espacio
+  //             child: ClipRRect(
+  //               borderRadius: BorderRadius.only(
+  //                 topLeft: Radius.circular(15),
+  //                 topRight: Radius.circular(15),
+  //               ),
+  //               child: Container(
+  //                 width: double.infinity,
+  //                 color: Colors.white, // Fondo blanco en caso de imagen nula
+  //                 child: product.image1 != null && product.image1!.isNotEmpty
+  //                     ? FadeInImage(
+  //                   image: NetworkImage(product.image1!),
+  //                   placeholder: AssetImage('assets/img/loading.png'),
+  //                   fit: BoxFit.contain, // Para que la imagen no se recorte
+  //                   fadeInDuration: Duration(milliseconds: 300),
+  //                   imageErrorBuilder: (context, error, stackTrace) {
+  //                     return Image.asset(
+  //                       'assets/img/no-image-icon.png',
+  //                       fit: BoxFit.contain,
+  //                     );
+  //                   },
+  //                 )
+  //                     : Image.asset(
+  //                   'assets/img/no-image-icon.png',
+  //                   fit: BoxFit.contain,
+  //                 ),
+  //               ),
+  //             ),
+  //           ),
+  //           SizedBox(height: 10), // Espacio antes del nombre
+  //           Padding(
+  //             padding: EdgeInsets.symmetric(horizontal: 10),
+  //             child: Text(
+  //               product.name ?? 'Sin nombre',
+  //               maxLines: 2,
+  //               overflow: TextOverflow.ellipsis,
+  //               style: TextStyle(
+  //                 fontSize: 16,
+  //                 fontWeight: FontWeight.bold,
+  //               ),
+  //             ),
+  //           ),
+  //           SizedBox(height: 5),
+  //           Padding(
+  //             padding: EdgeInsets.symmetric(horizontal: 10),
+  //             child: Text(
+  //               '${product.price ?? 0} Gs.',
+  //               style: TextStyle(
+  //                 fontSize: 14,
+  //                 color: Colors.green[700],
+  //                 fontWeight: FontWeight.w600,
+  //               ),
+  //             ),
+  //           ),
+  //           Spacer(), // Asegura que el botón quede alineado abajo
+  //           Align(
+  //             alignment: Alignment.bottomRight,
+  //             child: Container(
+  //               margin: EdgeInsets.only(right: 10, bottom: 10),
+  //               decoration: BoxDecoration(
+  //                 color: AppColors.primaryColor,
+  //                 shape: BoxShape.circle,
+  //                 boxShadow: [
+  //                   BoxShadow(
+  //                     color: Colors.black26,
+  //                     blurRadius: 4,
+  //                     offset: Offset(0, 2),
+  //                   ),
+  //                 ],
+  //               ),
+  //               child: IconButton(
+  //                 icon: Icon(Icons.add, color: Colors.white, size: 20), // Tamaño menor
+  //                 onPressed: () {
+  //                   // Acción de agregar producto
+  //                 },
+  //               ),
+  //             ),
+  //           ),
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
+
+
+  //ORIGINAL OPTION CARD PRODUCT
+  // Widget _cardProduct(Product product) {
+  //   return Container(
+  //     height: 250,
+  //     child: Card(
+  //       elevation: 3.0,
+  //       shape: RoundedRectangleBorder(
+  //         borderRadius: BorderRadius.circular(15)
+  //       ),
+  //       child: Stack(
+  //         children: [
+  //           Positioned(
+  //               top: -1.0,
+  //               right: -1.0,
+  //               child: Container(
+  //                 width: 40,
+  //                 height: 40,
+  //                 decoration: BoxDecoration(
+  //                   color: AppColors.primaryColor,
+  //                   borderRadius: BorderRadius.only(
+  //                     bottomLeft: Radius.circular(15),
+  //                     topRight: Radius.circular(20)
+  //                   )
+  //                 ),
+  //                 child: Icon(Icons.add, color: Colors.white),
+  //               )
+  //           ),
+  //           Column(
+  //             //el recycler view que hace que se carguen los productos por categoría que hay en la base de datos
+  //             crossAxisAlignment: CrossAxisAlignment.start,
+  //             children: [
+  //               Container(
+  //                 height: 150,
+  //                 margin: EdgeInsets.only(top: 20),
+  //                 width: MediaQuery.of(context).size.width * 0.45,
+  //                 padding: EdgeInsets.all(10),
+  //                 child: FadeInImage(
+  //                     image: product.image1 != null
+  //                       ? NetworkImage(product.image1!)
+  //                       : AssetImage('assets/img/burger.png'),
+  //                     fit: BoxFit.contain,
+  //                     fadeInDuration: Duration(milliseconds: 50),
+  //                     placeholder: AssetImage('assets/img/no-image-icon.png'),
+  //                 ),
+  //               ),
+  //               Container(
+  //                 margin: EdgeInsets.symmetric(horizontal: 20),
+  //                 height: 37,
+  //                 child: Text(
+  //                   product.name ?? '',
+  //                   maxLines: 2,
+  //                   overflow: TextOverflow.ellipsis,
+  //                   style: TextStyle(
+  //                     fontSize: 15,
+  //                     //fontStyle: 'Nimbusans'
+  //                   ),
+  //                 ),
+  //               ),
+  //               Spacer(),
+  //               Container(
+  //                 margin: EdgeInsets.symmetric(horizontal: 20),
+  //                 child: Text('${product.price ?? 0} \Gs.',
+  //                   style: TextStyle(
+  //                     fontSize: 15,
+  //                     fontWeight: FontWeight.bold
+  //                     //fontFamily: ''
+  //                   ),
+  //                 ),
+  //               )
+  //             ],
+  //           )
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
 
   Widget _menuDrawer() {
     return GestureDetector(
@@ -335,34 +548,70 @@ class _ClientProductsListPageState extends State<ClientProductsListPage> {
   Widget _textFieldSearch() {
     return Container(
       margin: EdgeInsets.symmetric(horizontal: 20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(25),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 4,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
       child: TextField(
         decoration: InputDecoration(
-          hintText: 'Buscar',
-          suffixIcon: Icon(
-              Icons.search_rounded,
-            color: Colors.grey[400]
-          ),
-          hintStyle: TextStyle(
-            fontSize: 17,
-            color: Colors.grey[500]
-          ),
+          hintText: 'Buscar...',
+          prefixIcon: Icon(Icons.search, color: Colors.grey[600]),
+          hintStyle: TextStyle(fontSize: 16, color: Colors.grey[500]),
+          filled: true,
+          fillColor: Colors.white,
           enabledBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(25),
-            borderSide: BorderSide(
-              color: Colors.grey
-            )
+            borderSide: BorderSide(color: Colors.transparent),
           ),
           focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(25),
-              borderSide: BorderSide(
-                  color: Colors.grey
-              )
+            borderRadius: BorderRadius.circular(25),
+            borderSide: BorderSide(color: Colors.grey.shade400),
           ),
-          contentPadding: EdgeInsets.all(15)
+          contentPadding: EdgeInsets.all(14),
         ),
       ),
     );
   }
+
+  //ORIGINAL OPTION
+  // Widget _textFieldSearch() {
+  //   return Container(
+  //     margin: EdgeInsets.symmetric(horizontal: 20),
+  //     child: TextField(
+  //       decoration: InputDecoration(
+  //         hintText: 'Buscar',
+  //         suffixIcon: Icon(
+  //             Icons.search_rounded,
+  //           color: Colors.grey[400]
+  //         ),
+  //         hintStyle: TextStyle(
+  //           fontSize: 17,
+  //           color: Colors.grey[500]
+  //         ),
+  //         enabledBorder: OutlineInputBorder(
+  //           borderRadius: BorderRadius.circular(25),
+  //           borderSide: BorderSide(
+  //             color: Colors.grey
+  //           )
+  //         ),
+  //         focusedBorder: OutlineInputBorder(
+  //             borderRadius: BorderRadius.circular(25),
+  //             borderSide: BorderSide(
+  //                 color: Colors.grey
+  //             )
+  //         ),
+  //         contentPadding: EdgeInsets.all(15)
+  //       ),
+  //     ),
+  //   );
+  // }
 
 /*Widget _drawer() {
     return Drawer (
