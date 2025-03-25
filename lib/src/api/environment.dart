49a5f6281632450a2c32c763ff56ed3cd3b0e867
Leticia Peña <leticia.penhag@fpuna.edu.py>
2025-03-24 21:21:02 -0300

    opcion para mostrar cuando no hay productos por categoría

--- lib/src/api/environment.dart
+++ lib/src/api/environment.dart
@@ -1,5 +1,5 @@
 class Environment {
-  static const String API_DELIVERY = "192.168.100.33:3000";
+  static const String API_DELIVERY = "192.168.100.5:3000";
   //static const String API_DELIVERY = "10.13.56.162:3000";
   //static const String API_DELIVERY = "192.168.100.5:3000";
 }
---
+++ lib/src/widgets/no_data_widget.dart
@@ -0,0 +1,28 @@
+
+import 'package:flutter/material.dart';
+
+//Para elementos estáticos
+import 'package:flutter/material.dart';
+
+// Para elementos estáticos
+class NoDataWidget extends StatelessWidget {
+  final String text; // Debe ser final
+
+  const NoDataWidget({Key? key, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 60),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('assets/img/no_items.png'),
+          SizedBox(height: 10),
+          Text(text, style: TextStyle(fontSize: 16)),
+        ],
+      ),
+    );
+  }
+}
+
