31d613cc95832f814e08a8474baca3df03bd201f
Leticia Peña <leticia.penhag@fpuna.edu.py>
2025-04-07 21:53:02 -0300

    mi orden page, wip

--- lib/src/pages/client/orders/create/client_orders_create_page.dart
+++ lib/src/pages/client/orders/create/client_orders_create_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:lomi_chef_to_go/src/pages/client/products/list/client_products_list_controller.dart';
+import 'package:lomi_chef_to_go/src/widgets/no_data_widget.dart';
 
 import '../../../../models/product.dart';
 import 'client_orders_create_controller.dart';
@@ -31,17 +32,30 @@ class _ClientOrdersCreatePageState extends State<ClientOrdersCreatePage> {
       appBar: AppBar(
         title: Text('Mi orden'),
       ),
-      body: Center(child: Text('Order')),
+      body: _controller.selectedProducts.isNotEmpty
+          ? ListView(
+        children: _controller.selectedProducts.map((Product product)  {
+          return _cardProduct(product);
+      }).toList(),
+      )   :
+      NoDataWidget(text: 'Ningún producto fue agregado',),
     );
+      
   }
 
   Widget _cardProduct(Product product) {
     return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
       child: Row(
         children: [
           Container(
             width: 90,
             height: 90,
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+              color: Colors.grey[200]
+            ),
             child: FadeInImage(
               image: product.image1 != null
                   ? NetworkImage(product.image1!)
