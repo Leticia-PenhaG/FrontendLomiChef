48baf1870eeed920b5a04145b461ca5a750a5c02
Leticia Peña <leticia.penhag@fpuna.edu.py>
2025-04-07 23:03:59 -0300

    primer diseño de la pantalla orden

--- lib/src/pages/client/orders/create/client_orders_create_page.dart
+++ lib/src/pages/client/orders/create/client_orders_create_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
-import 'package:lomi_chef_to_go/src/pages/client/products/list/client_products_list_controller.dart';
+import 'package:lomi_chef_to_go/src/utils/app_colors.dart';
 import 'package:lomi_chef_to_go/src/widgets/no_data_widget.dart';
 
 import '../../../../models/product.dart';
@@ -14,8 +14,8 @@ class ClientOrdersCreatePage extends StatefulWidget {
 }
 
 class _ClientOrdersCreatePageState extends State<ClientOrdersCreatePage> {
-
-  ClientsOrdersCreateController _controller = new ClientsOrdersCreateController();
+  ClientsOrdersCreateController _controller =
+      new ClientsOrdersCreateController();
 
   @override
   void initState() {
@@ -32,15 +32,30 @@ class _ClientOrdersCreatePageState extends State<ClientOrdersCreatePage> {
       appBar: AppBar(
         title: Text('Mi orden'),
       ),
+      bottomNavigationBar: Container(
+        height: MediaQuery.of(context).size.height * 0.235,
+        child: Column(
+          children: [
+            Divider(
+              color: Colors.grey[400],
+              endIndent: 30, //para definir margen derecha
+              indent: 30 //margen en la izquierda,
+            ),
+            _textTotalPrice(),
+            _buttonNext()
+          ],
+        ),
+      ),
       body: _controller.selectedProducts.isNotEmpty
           ? ListView(
-        children: _controller.selectedProducts.map((Product product)  {
-          return _cardProduct(product);
-      }).toList(),
-      )   :
-      NoDataWidget(text: 'Ningún producto fue agregado',),
+              children: _controller.selectedProducts.map((Product product) {
+                return _cardProduct(product);
+              }).toList(),
+            )
+          : NoDataWidget(
+              text: 'Ningún producto fue agregado',
+            ),
     );
-      
   }
 
   Widget _cardProduct(Product product) {
@@ -48,30 +63,149 @@ class _ClientOrdersCreatePageState extends State<ClientOrdersCreatePage> {
       margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
       child: Row(
         children: [
-          Container(
-            width: 90,
-            height: 90,
-            padding: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(20)),
-              color: Colors.grey[200]
-            ),
-            child: FadeInImage(
-              image: product.image1 != null
-                  ? NetworkImage(product.image1!)
-                  : AssetImage('assets/img/no-image-icon.png') as ImageProvider,
-              fit: BoxFit.contain,
-              placeholder: AssetImage('assets/img/no-image-icon.png'),
-              fadeInDuration: Duration(milliseconds: 50),
-            ),
-      )
-      ],
+          _imageProduct(product),
+          SizedBox(width: 10),
+          Column(
+            children: [
+              Text(
+                product.name ?? '',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10),
+              _addOrRemoveItem(product)
+            ],
+          ),
+          Spacer(),
+          Column(
+            children: [
+              _textPrice(product),
+              _iconDelete(product)
+            ],
+          )
+        ],
       ),
     );
   }
 
-  void refresh(){
-    setState(() {
-    });
+  Widget _iconDelete(Product product) {
+    return IconButton(
+        onPressed: () {},
+        icon: Icon(Icons.delete, color: AppColors.primaryColor,)
+    );
+  }
+
+  Widget _textPrice(Product product) {
+    return Container(
+      margin: EdgeInsets.only(top: 10),
+      child: Text(
+          '${product.price! * product.quantity!}',
+        style: TextStyle(
+          color: Colors.grey,
+          fontWeight: FontWeight.bold
+        ),
+      ),
+    );
+  }
+
+  Widget _imageProduct(Product product) {
+    return Container(
+      width: 90,
+      height: 90,
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+          color: Colors.grey[200]),
+      child: FadeInImage(
+        image: product.image1 != null
+            ? NetworkImage(product.image1!)
+            : AssetImage('assets/img/no-image-icon.png') as ImageProvider,
+        fit: BoxFit.contain,
+        placeholder: AssetImage('assets/img/no-image-icon.png'),
+        fadeInDuration: Duration(milliseconds: 50),
+      ),
+    );
+  }
+
+  Widget _addOrRemoveItem(Product product){
+    return Row(
+      children: [
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 12, vertical: 7),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(8),
+                bottomLeft: Radius.circular(8)
+            ),
+            color: Colors.grey[200],
+          ),
+          child: Text('-'),
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 12, vertical: 7),
+          color: Colors.grey[200],
+          child: Text('${product?.quantity ?? 0}'),
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 12, vertical: 7),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(8),
+                bottomRight: Radius.circular(8)
+            ),
+            color: Colors.grey[200],
+          ),
+          child: Text('+'),
+        ),
+      ],
+    );
+  }
+
+  Widget _textTotalPrice() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            'Total:',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 22
+            ),
+          ),
+          Text(
+            '0\Gs.',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+
+  Widget _buttonNext() {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primaryColor,
+        padding: EdgeInsets.symmetric(vertical: 15),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(Icons.check_circle, color: Colors.greenAccent, size: 30,),
+          SizedBox(width: 8),
+          Text('Continuar', style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold)),
+        ],
+      ),
+    );
+  }
+
+  void refresh() {
+    setState(() {});
   }
 }
