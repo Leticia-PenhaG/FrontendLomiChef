8f00e904896636e0fbf887324ea57262e4b22ef4
Leticia Peña <leticia.penhag@fpuna.edu.py>
2025-05-03 23:34:53 -0300

    se agrega página con la información de entrega

--- lib/src/pages/delivery/orders/map/delivery_orders_map_page.dart
+++ lib/src/pages/delivery/orders/map/delivery_orders_map_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+import '../../../../utils/app_colors.dart';
 import 'delivery_orders_map_controller.dart';
 
 class DeliveryOrdersMapPage extends StatefulWidget {
@@ -28,17 +29,24 @@ class _DeliveryOrdersMapPageState extends State<DeliveryOrdersMapPage> {
       body: SafeArea(
         child: Stack(
           children: [
-            _googleMaps(),
+            Container
+            (
+                height: MediaQuery.of(context).size.height * 0.6,
+                child: _googleMaps()
+            ),
             // Align(
             //   alignment: Alignment.center,
             //   child: _iconActualLocation(),
             // ),
-            Positioned(
-              bottom: 30,
-              left: 20,
-              right: 20,
-              child: _buttonAccept(),
-            )
+            SafeArea(
+                child: Column(
+              children:
+              [
+                _buttonCenterPosition(),
+                Spacer(), //UBICA ABAJO
+                _cardOrderInfo(),
+              ],
+            ))
           ],
         ),
       ),
@@ -56,32 +64,166 @@ class _DeliveryOrdersMapPageState extends State<DeliveryOrdersMapPage> {
     );
   }
 
-  // Widget _iconActualLocation() {
-  //   return Image.asset(
-  //     'assets/img/location_map_pin_mark_icon.png',
-  //     width: 65,
-  //     height: 65,
+  Widget _buttonCenterPosition() {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        alignment: Alignment.centerRight,
+        margin: EdgeInsets.symmetric(vertical:10, horizontal: 10),
+        child: Card(
+          shape: CircleBorder(),
+          child: Container(
+            padding: EdgeInsets.all(10),
+            child: Icon(
+              Icons.location_searching,
+              color:Colors.grey[800],
+                size: 20,
+            )
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _cardOrderInfo() {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.4 ,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(30),
+          topLeft: Radius.circular(30),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: Offset(0,3)
+          )
+        ]
+      ),
+      child: Column(
+        children: [
+          _listTitleAddress(_controller.order?.address?['neighborhood'], 'Barrio', Icons.my_location_outlined),
+          _listTitleAddress(_controller.order?.address?['address'], 'Dirección', Icons.location_on_outlined),
+          Divider(color: Colors.grey[400], endIndent: 30, indent: 30),
+          _clientInfo(),
+          Positioned(
+            bottom: 30,
+            left: 20,
+            right: 20,
+            child: _buttonAccept(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _listTitleAddress(String title, String subtitle, IconData iconData) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 30),
+      child: ListTile(
+        title: Text(
+          title ?? '',
+          style: TextStyle(
+              fontSize: 13
+          ),
+        ),
+        subtitle: Text(subtitle),
+        trailing: Icon(iconData),
+      ),
+    );
+  }
+  Widget _clientInfo() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 35, vertical: 20),
+      child: Row(
+        children: [
+          Container(
+            height: 50,
+            width: 50,
+            child: ClipRRect(
+              borderRadius: BorderRadius.vertical(top: Radius.circular(15)),
+              child: FadeInImage(
+                image: _controller.order?.client?['image'] != null
+                    ? NetworkImage(_controller.order?.client?['image'])
+                    : AssetImage('assets/img/no-image-icon.png') as ImageProvider,
+                fit: BoxFit.cover,
+                placeholder: AssetImage('assets/img/no-image-icon.png'),
+                fadeInDuration: Duration(milliseconds: 50),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 10),
+            child: Text
+            (
+              '${_controller.order?.client?['name'] ?? ''}' '${_controller.order?.client?['lastname'] ?? ''}',
+              style: TextStyle(
+                fontSize: 16
+              ),
+              maxLines: 1,
+            ),
+          ),
+          Spacer(),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+              color: Colors.grey[200]
+            ),
+            child: IconButton
+            (
+              onPressed: () {},
+              icon: Icon(Icons.phone_android, color:Colors.black)
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  // Widget _buttonAccept() {
+  //   return SizedBox(
+  //     height: 50,
+  //     child: ElevatedButton.icon(
+  //       onPressed: (){},
+  //       icon: const Icon(Icons.location_on, color: Colors.white),
+  //       label: const Text(
+  //         'Entregar producto',
+  //         style: TextStyle(
+  //           color: Colors.white,
+  //           fontSize: 16,
+  //           fontWeight: FontWeight.bold,
+  //         ),
+  //       ),
+  //       style: ElevatedButton.styleFrom(
+  //         backgroundColor: Colors.blueAccent,
+  //         shape: RoundedRectangleBorder(
+  //           borderRadius: BorderRadius.circular(14),
+  //         ),
+  //       ),
+  //     ),
   //   );
   // }
 
   Widget _buttonAccept() {
-    return SizedBox(
-      height: 50,
-      child: ElevatedButton.icon(
-        onPressed: _controller.selectReferencePoint,
-        icon: const Icon(Icons.location_on, color: Colors.white),
-        label: const Text(
-          'Seleccionar este punto',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+      child: SizedBox(
+        width: double.infinity,
+        height: 50,
+        child: ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: AppColors.primaryColor,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            elevation: 5,
           ),
-        ),
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.blueAccent,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(14),
+          child: const Text(
+            'Entregar producto',
+            style: TextStyle(fontSize: 18, color: Colors.white, fontWeight: FontWeight.bold),
           ),
         ),
       ),
