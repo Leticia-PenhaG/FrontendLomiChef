a65722885f2f1917a149901ac4cecf65a760a92f
Leticia Peña <leticia.penhag@fpuna.edu.py>
2025-06-21 08:28:17 -0300

    environment update

--- lib/src/api/environment.dart
+++ lib/src/api/environment.dart
@@ -1,7 +1,5 @@
 class Environment {
-  static const String API_DELIVERY = "192.168.100.5:3000";
-  //static const String API_DELIVERY = "10.13.56.162:3000";
-  //static const String API_DELIVERY = "192.168.100.33:3000";
-  //static const String API_KEY_GOOGLE_MAPS = "AIzaSyCU51c7EsEyE1_pTXrPsDdMMrpqjstx_pI";
-  static const String API_KEY_GOOGLE_MAPS = "AIzaSyADHClcjUO9FiXxuIaOdM_nau9YVUny8k8";
+  /*Borré los datos reales por seguridad, ya que el repositorio es ahora público*/
+  static const String API_DELIVERY = ""; //COLOCAR IP DE LA RED A LA QUE SE CONECTA
+  static const String API_KEY_GOOGLE_MAPS = ""; //COLOCAR API KEY GOOGLE MAPS
 }
